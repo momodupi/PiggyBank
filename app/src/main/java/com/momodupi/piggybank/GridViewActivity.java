b4560faff19338df88ab6f8359ee5bebb19a290a
momodupi <momodupi@gmail.com>
2019-07-25 00:53:19 -0400

    General framework

---
+++ app/src/main/java/com/momodupi/piggybank/GridViewActivity.java
@@ -0,0 +1,57 @@
+package com.momodupi.piggybank;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class GridViewActivity extends BaseAdapter {
+    private Context mContext;
+    private final String[] gridViewString;
+    private final int[] gridViewImageId;
+
+    public GridViewActivity(Context context, String[] gridViewString, int[] gridViewImageId) {
+        mContext = context;
+        this.gridViewImageId = gridViewImageId;
+        this.gridViewString = gridViewString;
+    }
+
+    @Override
+    public int getCount() {
+        return gridViewString.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+        View gridViewAndroid;
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+
+            gridViewAndroid = new View(mContext);
+            gridViewAndroid = inflater.inflate(R.layout.type_grid, null);
+            TextView textViewAndroid = (TextView) gridViewAndroid.findViewById(R.id.gridtext);
+            ImageView imageViewAndroid = (ImageView) gridViewAndroid.findViewById(R.id.gridimage);
+            textViewAndroid.setText(gridViewString[i]);
+            imageViewAndroid.setImageResource(gridViewImageId[i]);
+        } else {
+            gridViewAndroid = (View) convertView;
+        }
+
+        return gridViewAndroid;
+    }
+}
--- app/src/main/java/com/momodupi/piggybank/MainActivity.java
+++ app/src/main/java/com/momodupi/piggybank/MainActivity.java
@@ -1,10 +1,14 @@
 package com.momodupi.piggybank;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.core.view.GravityCompat;
@@ -20,9 +24,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static android.widget.GridView.*;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    String[] gridViewString = {
+            "Alram", "Android", "Mobile", "Website", "Profile", "WordPress",
+    };
+
+    int[] gridViewImageId = {
+            R.mipmap.baseline_drive_eta_black_48, R.mipmap.baseline_drive_eta_black_48,
+            R.mipmap.baseline_restaurant_black_48, R.mipmap.baseline_restaurant_black_48,
+            R.mipmap.baseline_restaurant_black_48, R.mipmap.baseline_restaurant_black_48,
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +59,45 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        GridViewActivity tpyrgridview_act = new GridViewActivity(MainActivity.this, gridViewString, gridViewImageId);
+        GridView tpyrgridview = (GridView) findViewById(R.id.type_grid);
+        tpyrgridview.setAdapter(tpyrgridview_act);
+
+        tpyrgridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "press" + position + "!", Toast.LENGTH_SHORT).show();
             }
         });
+
+
+        final EditText num_text = (EditText) findViewById(R.id.input_edittext);
+
+        final Button saveBtn = (Button) findViewById(R.id.input_btn);
+        saveBtn.requestFocus();
+
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view != null) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
+
+                String str = num_text.getText().toString();
+                if (str != null) {
+                    Toast.makeText(MainActivity.this, "press " + str + "!", Toast.LENGTH_SHORT).show();
+                    num_text.setText(null);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "null", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -45,6 +105,7 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
